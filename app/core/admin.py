6935d751d9d1114710ab13867165b6d7a94eaf0b
Maciej <maciej.choromanski@wp.pl>
2020-08-15 12:59:06 +0200

    Register models

--- app/core/admin.py
+++ app/core/admin.py
@@ -29,3 +29,6 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.FriendRequest)
+admin.site.register(models.Friend)
+admin.site.register(models.Message)
